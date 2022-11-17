1f5ccaed232cf01a1c673f4274ec396daea30ad6
Kaito Suzuki <snails8d@gmail.com>
2022-11-17 20:58:19 +0900

    add: oakで動くように調整

--- deno/deps.ts
+++ deno/deps.ts
@@ -1,1 +1,2 @@
 export { serve } from "https://deno.land/std@0.77.0/http/server.ts";
+export { Application } from "https://deno.land/x/oak@v6.5.0/mod.ts";
--- deno/src/main.ts
+++ deno/src/main.ts
@@ -1,10 +1,22 @@
 import { serve } from "../deps.ts";
+import { Application } from "../deps.ts";
 
 const PORT = 8000;
-const s = serve(`0.0.0.0:${PORT}`);
-const body = new TextEncoder().encode("Hello World\n");
 
-console.log(`Server started on port ${PORT}`);
-for await (const req of s) {
-  req.respond({ body });
-}
+// ---- http 
+// const s = serve(`0.0.0.0:${PORT}`);
+// const body = new TextEncoder().encode("Hello World\n");
+
+// console.log(`Server started on port ${PORT}`);
+// for await (const req of s) {
+//   req.respond({ body });
+// }
+
+
+// ork
+const server = new Application();
+
+server.use((ctx: any) => {
+  ctx.response.body = 'Hello World! oak';
+});
+await server.listen({ port: PORT });
