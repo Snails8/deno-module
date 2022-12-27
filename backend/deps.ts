4693e1418f3c07c5483b0d5e9e98a6ec4dca1f7e
Kaito Suzuki <snails8d@gmail.com>
2022-12-27 20:11:53 +0900

    add: 基本的なライブラリの追加

---
+++ backend/deps.ts
@@ -0,0 +1,25 @@
+export { serve } from "https://deno.land/std@0.165.0/http/server.ts";
+export {
+  Pool as DBPool,
+  PoolClient as DBConnection,
+  Transaction as DBTransaction,
+} from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+export * as pl from "https://deno.land/std@0.165.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.165.0/fs/mod.ts";
+export {
+  camelCase,
+  snakeCase,
+  upperFirst,
+} from "https://deno.land/x/lodash_es@v0.0.2/mod.ts";
+export * as yargs from "https://deno.land/x/yargs@v17.6.2-deno/deno.ts";
+export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+export {
+  Application,
+  Context,
+  Router,
+  Status,
+} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+export {
+  assertEquals,
+  assertArrayContains,
+} from "https://deno.land/std@0.65.0/testing/asserts.ts";
